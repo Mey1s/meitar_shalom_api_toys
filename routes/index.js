0323e7bad757bc9cbcb0f015caf5f8a5fa457658
m <meytar733@gmail.com>
2021-02-28 11:17:48 +0200

    master

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req,res) => {
-  res.json({message:"Toys API - created by Meitar Shalom. To see the documentation go to /documentation.html"});
+  res.json({message:"Toys API - created by Meitar Shalom. To see the documentation go to https://meitar-shalom-api-toys.herokuapp.com/documentation.html"});
 })
 
 module.exports = router;
